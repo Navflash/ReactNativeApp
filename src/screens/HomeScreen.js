3eb8b5b76953f1a2cb7feace9548bb4fe475e025
Navflash <rjnavneetsgr29@gmail.com>
2022-01-04 01:31:43 +0530

    navigation done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet,View,Button,TouchableOpacity, Touchable } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi Doctor</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+    <Text style={styles.text}>Hello peter</Text>
+    <Button
+      onPress={() => navigation.navigate('Components')}
+      title="Go To ComponentsDemo"
+    />
+    <Button
+      onPress={() => navigation.navigate('List')}
+      title="Go To ListDemo"
+    />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
